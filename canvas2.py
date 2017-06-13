2f349b3308d3b9cf291e50fd8f1c14b52997633a
osmithhisler <smithhislerol@slu.edu>
2017-06-13 15:40:49 -0500

    update 6/13/17 to canvas2.py
    Draws and randomly colors all four squares initially, after clicking with mouse the keyboard can be used to color a random square a chosen color, writes name of color and datetime to a canvas.bd file, and plays tone when red square is randomly generated in addition to when red is chosen using the r key on the keyboard

--- canvas2.py
+++ canvas2.py
@@ -1,26 +1,102 @@
-#draw three squares, randomly color each square, produce sound when red is generated, use keyboard to color the last square
+#draw four squares, randomly color each square, produce sound when red is generated, use keyboard to change color of random squares, write last color and datetime to .db file 
+import os, sys
 import random
+import time
 import winsound
 from Tkinter import *
 master = Tk()
 
 def fgreen(event):
-    w.create_rectangle(100,100,200,200, fill = "green")
+    s1 = [0,0,100,100]
+    s2 = [100,0,200,100]
+    s3 = [0,100,100,200]
+    s4 = [100,100,200,200]
+    s = [s1, s2, s3, s4]
+    w.create_rectangle(random.choice(s), fill = "green")
+    filename = "C:\\Users\\Olivia\\Desktop\\DUNE\\Python\\canvas2.db"
+    dirname = os.path.dirname(filename)
+    if not os.path.exists(dirname):
+        os.makedirs(dirname, 755)
+    file = open(filename, 'w')
+    file.write("green " + str(time.strftime("%Y/%m/%d %H:%M:%S")))
+    file.close()
 
 def fred(event):
-    w.create_rectangle(100,100,200,200, fill = "red")
+    s1 = [0,0,100,100]
+    s2 = [100,0,200,100]
+    s3 = [0,100,100,200]
+    s4 = [100,100,200,200]
+    s = [s1, s2, s3, s4]
+    w.create_rectangle(random.choice(s), fill = "red")
+    #play tone if keyboard is used to create a red square
+    winsound.Beep(275, 1000)
+    filename = "C:\\Users\\Olivia\\Desktop\\DUNE\\Python\\canvas2.db"
+    dirname = os.path.dirname(filename)
+    if not os.path.exists(dirname):
+        os.makedirs(dirname, 755)
+    file = open(filename, 'w')
+    file.write("red " + str(time.strftime("%Y/%m/%d %H:%M:%S")))
+    file.close()
     
 def forange(event):
-    w.create_rectangle(100,100,200,200, fill = "orange")
+    s1 = [0,0,100,100]
+    s2 = [100,0,200,100]
+    s3 = [0,100,100,200]
+    s4 = [100,100,200,200]
+    s = [s1, s2, s3, s4]
+    w.create_rectangle(random.choice(s), fill = "orange")
+    filename = "C:\\Users\\Olivia\\Desktop\\DUNE\\Python\\canvas2.db"
+    dirname = os.path.dirname(filename)
+    if not os.path.exists(dirname):
+        os.makedirs(dirname, 755)
+    file = open(filename, 'w')
+    file.write("orange " + str(time.strftime("%Y/%m/%d %H:%M:%S")))
+    file.close()
 
 def fyellow(event):
-    w.create_rectangle(100,100,200,200, fill = "yellow")
+    s1 = [0,0,100,100]
+    s2 = [100,0,200,100]
+    s3 = [0,100,100,200]
+    s4 = [100,100,200,200]
+    s = [s1, s2, s3, s4]
+    w.create_rectangle(random.choice(s), fill = "yellow")
+    filename = "C:\\Users\\Olivia\\Desktop\\DUNE\\Python\\canvas2.db"
+    dirname = os.path.dirname(filename)
+    if not os.path.exists(dirname):
+        os.makedirs(dirname, 755)
+    file = open(filename, 'w')
+    file.write("yellow " + str(time.strftime("%Y/%m/%d %H:%M:%S")))
+    file.close()
 
 def fblue(event):
-    w.create_rectangle(100,100,200,200, fill = "blue")
+    s1 = [0,0,100,100]
+    s2 = [100,0,200,100]
+    s3 = [0,100,100,200]
+    s4 = [100,100,200,200]
+    s = [s1, s2, s3, s4]
+    w.create_rectangle(random.choice(s), fill = "blue")
+    filename = "C:\\Users\\Olivia\\Desktop\\DUNE\\Python\\canvas2.db"
+    dirname = os.path.dirname(filename)
+    if not os.path.exists(dirname):
+        os.makedirs(dirname, 755)
+    file = open(filename, 'w')
+    file.write("blue " + str(time.strftime("%Y/%m/%d %H:%M:%S")))
+    file.close()
 
 def fviolet(event):
-    w.create_rectangle(100,100,200,200, fill = "violet")
+    s1 = [0,0,100,100]
+    s2 = [100,0,200,100]
+    s3 = [0,100,100,200]
+    s4 = [100,100,200,200]
+    s = [s1, s2, s3, s4]
+    w.create_rectangle(random.choice(s), fill = "violet")
+    filename = "C:\\Users\\Olivia\\Desktop\\DUNE\\Python\\canvas2.db"
+    dirname = os.path.dirname(filename)
+    if not os.path.exists(dirname):
+        os.makedirs(dirname, 755)
+    file = open(filename, 'w')
+    file.write("violet " + str(time.strftime("%Y/%m/%d %H:%M:%S")))
+    file.close()
 
 def mouseclick(event):
     w.focus_set()
@@ -28,13 +104,13 @@ def mouseclick(event):
 w = Canvas(master, width = 200, height = 200)
 
 #click in canvas and then use keyboard to color the fourth square
+w.bind("<Button-1>", mouseclick)
 w.bind("<g>", fgreen)
 w.bind("<r>", fred)
 w.bind("<o>", forange)
 w.bind("<y>", fyellow)
 w.bind("<b>", fblue)
 w.bind("<v>", fviolet)
-w.bind("<Button-1>", mouseclick)
 w.pack()
 
 #choose random colors from color list 
@@ -42,19 +118,21 @@ color = ["red", "orange", "yellow", "green", "blue", "violet"]
 c1 = random.choice(color)
 c2 = random.choice(color)
 c3 = random.choice(color)
+c4 = random.choice(color)
 
 #draw three more squares
 w.create_rectangle(0,0,100,100, fill = c1)
 w.create_rectangle(100,0,200,100, fill = c2)
 w.create_rectangle(0,100,100,200, fill = c3)
+w.create_rectangle(100,100,200,200, fill = c4)
 
 #add text
 w.create_text(150,125,text = "Click on canvas.")
 w.create_text(150,150,text = "and use keyboard")
 w.create_text(150,175,text = "to choose color.")
 
-#play tone when a red square is generated (but not if red is chosen by clicking 'r' key)
-if c1 == "red" or c2 == "red" or c3 == "red":
+#play tone when a red square is generated
+if c1 == "red" or c2 == "red" or c3 == "red" or c4 == "red":
     winsound.Beep(275, 1000)
-
+    
 master.mainloop()
